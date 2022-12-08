e4f3d89482c39423dda6da8864e748167673da01
implementacaoteste <106354515+implementacaoteste@users.noreply.github.com>
2022-12-08 10:06:46 -0300

    single render e multi render

--- contador_de_pessoas/lib/home_page.dart
+++ contador_de_pessoas/lib/home_page.dart
@@ -20,19 +20,17 @@ class HomePageState extends State {
           'Meu contador',
         ),
       ),
-      body: Center(
-          child: GestureDetector(
-        child: Text(
-          'Contador: $counter',
-          style: TextStyle(fontSize: 32),
-        ),
-        onTap: () {
-          setState(() {
-            counter++;
-          });
-          print(counter);
-        },
-      )),
+      body: Container(
+        height: 200,
+        width: 200,
+        color: Colors.black,
+        child: Center(
+            child: Container(
+          width: 110,
+          height: 110,
+          color: Colors.yellow,
+        )),
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
