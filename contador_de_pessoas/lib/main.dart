6062c20d8c6db90e5446310bd8082434e79bf429
implementacaoteste <106354515+implementacaoteste@users.noreply.github.com>
2022-12-08 09:18:39 -0300

    StatefulWidget

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -16,9 +16,34 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.yellow),
-      home: Container(
-        child: Center(child: Text('Erisvaldo')),
-      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: GestureDetector(
+        child: Text('Contador: $counter'),
+        onTap: () {
+          setState(() {
+            counter++;
+          });
+          print(counter);
+        },
+      )),
     );
   }
 }
