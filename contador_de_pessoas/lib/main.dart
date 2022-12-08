d9cc1610b01cdc5e8386482a78714690376a5344
implementacaoteste <106354515+implementacaoteste@users.noreply.github.com>
2022-12-08 08:51:13 -0300

    StatelessWidget

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -2,13 +2,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(Container(
-    child: const Center(
-      child: Text(
-        'asdf',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 100.0),
-      ),
-    ),
+  runApp(AppWidget(
+    title: 'ffff',
   ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  //const AppWidget({super.key, required this.title});
+  const AppWidget({Key? key, required this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: const TextStyle(color: Colors.white, fontSize: 100.0),
+        ),
+      ),
+    );
+  }
 }
